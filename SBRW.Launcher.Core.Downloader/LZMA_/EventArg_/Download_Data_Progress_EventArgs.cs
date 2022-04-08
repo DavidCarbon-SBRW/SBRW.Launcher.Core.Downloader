33488b3e43bff3a027fd7c163f6b498e2990b4c5
David L <open-source@davidcarbon.dev>
2022-04-08 11:16:11 -0700

    Fix Test Build

--- SBRW.Launcher.Core.Downloader/LZMA_/EventArg_/Download_Data_Progress_EventArgs.cs
+++ SBRW.Launcher.Core.Downloader/LZMA_/EventArg_/Download_Data_Progress_EventArgs.cs
@@ -45,7 +45,7 @@ namespace SBRW.Launcher.Core.Downloader.LZMA_.EventArg_
         /// <param name="Received_File_Size_Total"></param>
         /// <param name="Received_File_Size_Current"></param>
         /// <param name="Received_Start_Time"></param>
-        public Download_Data_Progress_EventArgs(long Received_File_Size_Current, long Received_File_Size_Total, long Received_File_Size_Total_Uncompressed long Calulated_Current_Divide_Total, long Calulated_Current_Divide_Total_Uncompressed, int Calulated_Percentage, DateTime Received_Start_Time)
+        public Download_Data_Progress_EventArgs(long Received_File_Size_Current, long Received_File_Size_Total, long Received_File_Size_Total_Uncompressed, long Calulated_Current_Divide_Total, long Calulated_Current_Divide_Total_Uncompressed, int Calulated_Percentage, DateTime Received_Start_Time)
         {
             this.File_Size_Total = Received_File_Size_Total;
             this.File_Size_Current = Received_File_Size_Current;
