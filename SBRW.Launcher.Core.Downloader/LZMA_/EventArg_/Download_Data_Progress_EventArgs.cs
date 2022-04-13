2627b07fcd657d0cfde654f8c33431e75e4b7536
David L <open-source@davidcarbon.dev>
2022-04-13 14:37:23 -0700

    Allow Programmer to set a Start Time for LZMA
    Added:
    - Start Time can now be provided instead of the downloader generating its own start time stamp when a download starts

--- SBRW.Launcher.Core.Downloader/LZMA_/EventArg_/Download_Data_Progress_EventArgs.cs
+++ SBRW.Launcher.Core.Downloader/LZMA_/EventArg_/Download_Data_Progress_EventArgs.cs
@@ -10,10 +10,6 @@ namespace SBRW.Launcher.Core.Downloader.LZMA_.EventArg_
         /// <summary>
         /// 
         /// </summary>
-        public int Download_Percentage { get; internal set; }
-        /// <summary>
-        /// 
-        /// </summary>
         public string? File_Name { get; internal set; }
         /// <summary>
         /// 
@@ -26,27 +22,19 @@ namespace SBRW.Launcher.Core.Downloader.LZMA_.EventArg_
         /// <summary>
         /// 
         /// </summary>
-        public decimal Bytes_Received_Over_Total { get; internal set; }
-        /// <summary>
-        /// 
-        /// </summary>
         public DateTime Start_Time { get; internal set; }
         /// <summary>
         /// 
         /// </summary>
         /// <param name="Received_Bytes_Current"></param>
         /// <param name="Received_Bytes_Total"></param>
-        /// <param name="Calulated_Current_Divide_Total"></param>
-        /// <param name="Calulated_Percentage"></param>
         /// <param name="Received_File_Name"></param>
         /// <param name="Received_Start_Time"></param>
-        public Download_Data_Progress_EventArgs(long Received_Bytes_Current, long Received_Bytes_Total, decimal Calulated_Current_Divide_Total, int Calulated_Percentage, string Received_File_Name, DateTime Received_Start_Time)
+        public Download_Data_Progress_EventArgs(long Received_Bytes_Current, long Received_Bytes_Total, string Received_File_Name, DateTime Received_Start_Time)
         {
             this.Bytes_To_Receive_Total = Received_Bytes_Total;
             this.Bytes_Received = Received_Bytes_Current;
-            this.Download_Percentage = Calulated_Percentage;
             this.Start_Time = Received_Start_Time;
-            this.Bytes_Received_Over_Total = Calulated_Current_Divide_Total;
             this.File_Name = Received_File_Name;
         }
     }
