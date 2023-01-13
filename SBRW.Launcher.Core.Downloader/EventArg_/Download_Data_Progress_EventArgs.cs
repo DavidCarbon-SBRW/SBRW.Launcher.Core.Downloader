a04f26ba3404cb40517b562c6f86a0ef6e7eb29c
DavidCarbon <open-source@davidcarbon.dev>
2023-01-13 02:17:06 -0800

    Fix file download stopping abruptly

--- SBRW.Launcher.Core.Downloader/EventArg_/Download_Data_Progress_EventArgs.cs
+++ SBRW.Launcher.Core.Downloader/EventArg_/Download_Data_Progress_EventArgs.cs
@@ -26,17 +26,24 @@ namespace SBRW.Launcher.Core.Downloader.EventArg_
         /// <summary>
         /// 
         /// </summary>
+        public long File_Size_Remaining { get; internal set; }
+        /// <summary>
+        /// 
+        /// </summary>
         public DateTime Start_Time { get; internal set; }
         /// <summary>
         /// 
         /// </summary>
         /// <param name="Received_File_Size_Total"></param>
         /// <param name="Received_File_Size_Current"></param>
+        /// <param name="Received_File_Size_Remaining"></param>
         /// <param name="Received_Start_Time"></param>
-        public Download_Data_Progress_EventArgs(long Received_File_Size_Total, long Received_File_Size_Current, DateTime Received_Start_Time)
+        public Download_Data_Progress_EventArgs(long Received_File_Size_Total, long Received_File_Size_Current,
+            long Received_File_Size_Remaining, DateTime Received_Start_Time)
         {
             this.File_Size_Total = Received_File_Size_Total;
             this.File_Size_Current = Received_File_Size_Current;
+            this.File_Size_Remaining = Received_File_Size_Remaining;
             this.Download_Percentage = (int)((((double)Received_File_Size_Current) / Received_File_Size_Total) * 100);
             this.Start_Time = Received_Start_Time;
         }
